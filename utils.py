201b2c9a59b0bd505743cf0bef9b4bdd486ae56d
yamato0811 <yama8to11@gmail.com>
2022-02-04 06:41:12 +0000

    add resize function

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-
 # 距離:2~0, 類似度:0~100
 def distance_to_similarity(dis, min_dis=0.25, max_dis=1.0):
     if dis < min_dis:
@@ -13,3 +12,9 @@ def distance_to_similarity(dis, min_dis=0.25, max_dis=1.0):
 def crop(img, box):
     x,y,width,hight = box
     return img[y:y+hight, x:x+width]
+
+def resize(img, Image, width=960, height=540):
+    if img.width > width or img.height > height:
+        #img = img.resize((960,540),img.BICUBIC)
+        img = img.resize((img.width//2,img.height//2),Image.BICUBIC)
+    return img
