c6150081d68385281b298339a8ad9b8a219c010e
yamato0811 <yama8to11@gmail.com>
2022-12-18 11:23:41 +0000

    Add judgment results

--- streamlit.py
+++ streamlit.py
@@ -10,19 +10,20 @@ from dataloader import Dataloader
 @st.cache(allow_output_mutation=True)
 def load_facenet():
     return FaceNet()
-
 @st.cache
 def load_dataloader():
     return Dataloader()
 
-
-st.title('Similarity with Hara')
-st.write("早速あなたの顔の画像をアップロードしてみましょう")
-
-uploaded_file = st.file_uploader('Choose a image file', type=['jpg','jpeg','png'])
-
 facenet = load_facenet()
 dataloader = load_dataloader()
+
+# CSSの読み込み
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+st.title('Similarity with HARA')
+st.write("早速あなたの顔の画像をアップロードしてみましょう")
+uploaded_file = st.file_uploader('Choose a image file', type=['jpg','jpeg','png'])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
@@ -30,11 +31,10 @@ if uploaded_file is not None:
     image = resize(image)
 
     img_array = np.array(image)
-
     extracts = facenet.extract(img_array)
 
-    # 顔が画像中に存在しないとき
     if len(extracts) < 1:
+    # 顔が画像中に存在しないとき
         st.header('Face detection faild')
         st.image(
             img_array, 
@@ -42,30 +42,31 @@ if uploaded_file is not None:
         )
     else:
         max_extract = max(extracts, key=lambda x:x['box'][2]*x['box'][3]) # もっとも大きい顔を取得
-
         embed_img = max_extract['embedding']
 
         distances = [(facenet.compute_distance(e, embed_img), p) for e, p in dataloader] # hara dataset内のすべての画像と距離を計算
         distance, path = min(distances)
         similarity = distance_to_similarity(distance)
-        st.subheader(f'Similarity : {round(similarity)} %')
-        st.text(f'distance : {round(distance, 2)}')
+        
+        # 判定結果
+        if similarity > 90:
+            st.text('You are HARA🎉🎉🎉')
+        else:
+            st.text('You are not HARA😞😞😞')
+        st.text(f'Similarity : {round(similarity)} %')
 
         img_array = crop(img_array, max_extract['box'])
-
         uploaded_img, picked_img = adjust_img_margin(img_array, dataloader.load_img(path))
 
         col1, col2 = st.columns(2)
-
         with col1:
-            st.header("Upload Image")
+            st.write("Upload Image")
             st.image(
                 uploaded_img, 
                 use_column_width=True
             )
-
         with col2:
-            st.header("Most Similar Hara")
+            st.write("Most Similar Hara")
             st.image(
                 picked_img, 
                 use_column_width=True
