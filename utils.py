da10da20b46bb2c9b0b40d127a3e5d40439f051f
yamato0811 <yama8to11@gmail.com>
2022-12-22 11:36:17 +0000

    update

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 # 距離:2~0, 類似度:0~100
-def distance_to_similarity(dis, min_dis=0.25, max_dis=1.0):
+def distance_to_similarity(dis, min_dis=0.2, max_dis=1.0):
     if dis < min_dis:
         similarity = 100
     elif dis > max_dis:
